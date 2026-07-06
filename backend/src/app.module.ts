129566768b43e2fdbbc030d56faf797d2108d849
Eduardo <ecarllos017@gmail.com>
2026-07-06 11:45:43 -0300

    feat(feature/db): add PrismaMModule e ResponsavelModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ResponsavelModule } from './responsavel/responsavel.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, ResponsavelModule],
   controllers: [AppController],
   providers: [AppService],
 })

4f630c5087b0a90ea505eeb6977e8c8652f0c963
Eduardo <ecarllos017@gmail.com>
2026-07-07 14:17:52 -0300

    feat(feature/responsavel): add create responsavel dto

--- backend/src/responsavel/dto/create-responsavel.dto.ts
+++ backend/src/responsavel/dto/create-responsavel.dto.ts
@@ -1,1 +1,31 @@
-export class CreateResponsavelDto {}
+import { Cidades, Setores } from '@prisma/client';
+import {
+  IsBoolean,
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
+export class CreateResponsavelDto {
+  @IsNotEmpty()
+  @IsString()
+  nome!: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email!: string;
+
+  @IsNotEmpty()
+  setor!: Setores;
+
+  @IsNotEmpty()
+  cidade!: Cidades;
+
+  @IsBoolean()
+  status!: boolean;
+
+  @IsOptional()
+  @IsString()
+  assinatura?: string;
+}

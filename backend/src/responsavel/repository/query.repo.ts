b55609424734f911ae6112de1483b59f946cc03b
Eduardo <ecarllos017@gmail.com>
2026-07-07 15:17:27 -0300

    feat(feature/responsavel): add function buscarEmail

---
+++ backend/src/responsavel/repository/query.repo.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@/prisma/prisma.service';
+
+@Injectable()
+export class ResponsavelQuery {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async buscarEmail(email: string) {
+    return await this.prisma.responsavel.findUnique({ where: { email } });
+  }
+}

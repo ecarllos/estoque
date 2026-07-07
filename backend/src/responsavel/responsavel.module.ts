44a3b50390086a191d45419c852bc28989f0933e
Eduardo <ecarllos017@gmail.com>
2026-07-07 15:20:51 -0300

    feat(feature/responsavel): add use-case, query & command de Responsavel

--- backend/src/responsavel/responsavel.module.ts
+++ backend/src/responsavel/responsavel.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ResponsavelService } from './responsavel.service';
 import { ResponsavelController } from './responsavel.controller';
+import { CriarResponsavelUseCase } from './use-cases/criar-responsavel.use-case';
+import { ResponsavelQuery } from './repository/query.repo';
+import { responsavelCommand } from './repository/command.repo';
 
 @Module({
   controllers: [ResponsavelController],
-  providers: [ResponsavelService],
+  providers: [CriarResponsavelUseCase, ResponsavelQuery, responsavelCommand],
 })
 export class ResponsavelModule {}

e9435868eccd3f0a7d05ddda823040605e040022
Eduardo <ecarllos017@gmail.com>
2026-07-08 10:26:52 -0300

    feat(feature/responsavel): add GET para mostrarUmResponsavel

--- backend/src/responsavel/responsavel.controller.ts
+++ backend/src/responsavel/responsavel.controller.ts
@@ -1,13 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { CreateResponsavelDto } from './dto/create-responsavel.dto';
 import { CriarResponsavelUseCase } from './use-cases/criar-responsavel.use-case';
+import { MostrarUmResponsavel } from './use-cases/mostrar-um-responsavel.use-case';
 
 @Controller('responsavel')
 export class ResponsavelController {
-  constructor(private readonly criarResponsavel: CriarResponsavelUseCase) {}
+  constructor(
+    private readonly criarResponsavel: CriarResponsavelUseCase,
+    private readonly mostrarUmResponsavel: MostrarUmResponsavel,
+  ) {}
 
   @Post('/create')
-  create(@Body() createResponsavelDto: CreateResponsavelDto) {
-    return this.criarResponsavel.execute(createResponsavelDto);
+  async create(@Body() dto: CreateResponsavelDto) {
+    return await this.criarResponsavel.execute(dto);
+  }
+
+  @Get(':email')
+  async mostrarUm(@Param('email') email: string) {
+    return await this.mostrarUmResponsavel.execute(email);
   }
 }

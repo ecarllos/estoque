7e2d26e83e745d6592becae241fb5a969892b788
Eduardo <ecarllos017@gmail.com>
2026-07-08 10:27:23 -0300

    feat(feature/responsavel): add  provider mostrarUmResponsavel

--- backend/src/responsavel/responsavel.module.ts
+++ backend/src/responsavel/responsavel.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { ResponsavelController } from './responsavel.controller';
 import { CriarResponsavelUseCase } from './use-cases/criar-responsavel.use-case';
 import { ResponsavelQuery } from './repository/query.repo';
-import { responsavelCommand } from './repository/command.repo';
+import { ResponsavelCommand } from './repository/command.repo';
+import { MostrarUmResponsavel } from './use-cases/mostrar-um-responsavel.use-case';
 
 @Module({
   controllers: [ResponsavelController],
-  providers: [CriarResponsavelUseCase, ResponsavelQuery, responsavelCommand],
+  providers: [
+    ResponsavelQuery,
+    ResponsavelCommand,
+    CriarResponsavelUseCase,
+    MostrarUmResponsavel,
+  ],
 })
 export class ResponsavelModule {}

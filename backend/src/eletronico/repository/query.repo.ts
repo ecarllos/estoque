770a364d44759b9e55cbd2f08dff517771dbc7a9
Eduardo <ecarllos017@gmail.com>
2026-07-12 16:06:57 -0300

    feat(feature/eletronico): add modelo piloto de query de listar os eletronicos

--- backend/src/eletronico/repository/query.repo.ts
+++ backend/src/eletronico/repository/query.repo.ts
@@ -4,4 +4,20 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class EletronicoQuery {
   constructor(private readonly prisma: PrismaService) {}
+
+  async listarEletronicos(pular: number, limite: number) {
+    return await this.prisma.eletronico.findMany({
+      skip: pular,
+      take: limite,
+      include: {
+        vinculo: {
+          select: {
+            id: true,
+            status: true,
+            responsavel: { select: { nome: true, email: true } },
+          },
+        },
+      },
+    });
+  }
 }

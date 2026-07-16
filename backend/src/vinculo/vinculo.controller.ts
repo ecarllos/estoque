8bf33d5dc7028667c5b451b24f735f7cf32e45af
Eduardo <ecarllos017@gmail.com>
2026-07-16 08:35:08 -0300

    feat(feature/vinculo): adiciona método @Post criarVinculo

--- backend/src/vinculo/vinculo.controller.ts
+++ backend/src/vinculo/vinculo.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CriarVinculoUseCase } from './use-cases/criar-vinculo.use-case';
+import { CreateVinculoDto } from './dto/create/createVinculo.dto';
 
 @Controller('vinculo')
-export class VinculoController {}
+export class VinculoController {
+  constructor(private readonly criar: CriarVinculoUseCase) {}
+  @Post('create')
+  async criarVinculo(@Body() dto: CreateVinculoDto) {
+    // console.log(dto);
+
+    return await this.criar.execute(dto);
+  }
+}
